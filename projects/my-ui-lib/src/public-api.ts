f779c449d7704d357bd1389d0cf3fee15252744b
Hataichanok Kochsena <pea@MacBook-Pro-khxng-Hataichanok.local>
2025-12-15 14:25:33 +0700

    feat(button): add button component with variants, sizes and icons

--- projects/my-ui-lib/src/public-api.ts
+++ projects/my-ui-lib/src/public-api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of my-ui-lib
  */
+export * from './lib/button/button.component';
 export * from './lib/searchfield/searchfield.component';
 export * from './lib/textfield/textfield.component';
 export * from './lib/my-ui-lib';
